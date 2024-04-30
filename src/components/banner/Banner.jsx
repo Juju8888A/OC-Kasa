9d634bc4286f8a28d402ad4cff1ee7815fc4f01b
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-04-30 15:23:22 +0200

    modif police montserrat + ajout h1

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div></div>
+    <div>
+        <h1>Chez vous, partout et ailleurs</h1>
+    </div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import Banner from '../components/Banner/Banner';
+import Card from '../components/Card/Card';
 
 const Home = () => {
   return(
     <div className='home-page'>
         <Header />
-        <h2>Page accueil</h2>
-        <main></main>
+        <main>
+          <Banner />
+          <Card />
+        </main>
         <Footer />
     </div>
 
