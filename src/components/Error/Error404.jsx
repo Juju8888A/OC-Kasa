86f5621b5314332830c379153ebb44a2562e770b
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-05-06 10:51:40 +0200

    style de la page error

--- src/components/Error/Error404.jsx
+++ src/components/Error/Error404.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 
 const Error404 = ({}) => {
   return (
-    <div>
+    <div className="error-container">
        <h2>404</h2>
        <p>Oups! La page que vous demandez n'existe pas.</p>
-       <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+       <NavLink to="/" className="accueil-link">Retourner sur la page d'accueil</NavLink>
     </div>
   );
 };
