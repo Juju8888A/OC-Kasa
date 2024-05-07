83c635d9ad6d5ac38f68e1eeeb36583f2eb1d94e
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-05-07 15:34:18 +0200

    mise en forme du carrousel, avec les fonctions image précédente, suivante et le compteur

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import arrowLeft from "../../assets/icones/arrow_left.png";
+import arrowRight from "../../assets/icones/arrow_right.png";
+import { useState } from "react";
 
-const Carrousel = () => {
+const Carrousel = ({ images }) => {
+  const [index, setIndex] = useState(0);
+
+  const previousImage = () => {
+    setIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const nextImage = () => {
+    setIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
     <div className="carrousel">
-       
+      <div className="image-carrousel">
+        {/* <img src={images[index]} alt={`Image de ${index}`} /> */}
+        </div>
+      <div className="btn-carrousel">
+        <button onClick={previousImage}>
+          <img src={arrowLeft} alt={`Image ${index + 1}`} />
+        </button>
+        <button onClick={nextImage}>
+          <img src={arrowRight} alt={`Image ${index + 1}`} />
+        </button>
+      </div>
+      <div className="slide-number">{index + 1} / 2 </div>
+
     </div>
   );
 };
