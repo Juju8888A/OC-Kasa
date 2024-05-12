80754a0c70a4fd466d48a54aace1b0b0fb315d6e
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-05-12 12:35:12 +0200

    redirection vers la page error, création des étoiles de notation des appartements

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -5,27 +5,27 @@ import Footer from "../components/Footer/Footer";
 import Carrousel from "../components/Carrousel/Carrousel";
 import Collapse from "../components/Collapse/Collapse";
 import location from "../data/data-location.json";
+import Error from "./Error";
+import greyStar from "../assets/icones/grey_star.png";
+import redStar from "../assets/icones/red_star.png";
 
 const Location = () => {
   const { id } = useParams();
-  console.log({ id })
+  console.log({ id });
 
   // je filtre les données pour afficher uniquement les informations de l'appartement correspondant à l'ID
-  const apartment = location.find(element => element.id === id);
+  const apartment = location.find((element) => element.id === id);
 
   if (!apartment) {
-    return (
-      <div className="location-page">
-        <Header />
-        <main>
-          <div className="error-message">
-            <p>L'appartement que vous recherchez n'existe pas.</p>
-          </div>
-        </main>
-        <Footer />
-      </div>
-    );
+    return <Error />;
   }
+
+  // je génère les 5 étoiles de notation
+  const stars = [1, 2, 3, 4, 5];
+  // je souhaite que ma fonction affiche les étoiles, rouges si true, grey si false
+  const displayStars = (star) => {
+    return <img src={star ? redStar : greyStar} alt="étoile" />;
+  };
 
   return (
     <div className="location-page">
@@ -39,23 +39,44 @@ const Location = () => {
               <h2>{apartment.location}</h2>
               <div>
                 <ul>
-                  <li>{apartment.tags.map((tag, index) => <li key={index}>{tag}</li>)}</li>
+                  <li>
+                    {apartment.tags.map((tag, index) => (
+                      <li key={index}>{tag}</li>
+                    ))}
+                  </li>
                 </ul>
               </div>
             </div>
             <div className="info-proprio">
               <div className="name-proprio">
                 <p>{apartment.host.name}</p>
-                <img src={apartment.host.picture} alt={`photo de ` + apartment.host.name}></img>
+                <img
+                  src={apartment.host.picture}
+                  alt={`photo de ` + apartment.host.name}
+                ></img>
               </div>
               <div className="stars">
-                <img src="" alt=""></img>
+                {/* génération des étoiles en fonction du rating de l'appartement, détermine si c'est l'étoile rouge ou grise qui doit apparaître */}
+                {stars.map((star, index) => (
+                  <span key={index}>
+                    {displayStars(star <= apartment.rating)}
+                  </span>
+                ))}
               </div>
             </div>
           </div>
           <div className="collapse-location">
             <Collapse title="Description" content={apartment.description} />
-            <Collapse title="Equipements" content={<ul>{apartment.equipments.map((equipement, index) => <li key={index}>{equipement}</li>)}</ul>} />
+            <Collapse
+              title="Equipements"
+              content={
+                <ul>
+                  {apartment.equipments.map((equipement, index) => (
+                    <li key={index}>{equipement}</li>
+                  ))}
+                </ul>
+              }
+            />
           </div>
         </div>
       </main>
